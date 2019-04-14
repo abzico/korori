e1ae75ef267145d65326a99a6da64146afc50083
Wasin Thonkaew <haxpor@gmail.com>
2019-04-15 06:03:01 +0800

    Make it empty function for print stack trace util function for mobile
    On arm, we should use unwind.h instead, will add this in future.

--- src/foundation/util.c
+++ src/foundation/util.c
@@ -1,8 +1,11 @@
 #include "krr/foundation/util.h"
+#include "krr/platforms/platforms_config.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-#if defined __APPLE__ || defined __linux__
+// FIXME: On arm refer to https://stackoverflow.com/questions/3398664/how-to-get-a-call-stack-backtrace-deeply-embedded-no-library-support to use unwind.h header with similar functionality. For now when calling KRR_util_print_callstack() on unsupported platform,
+// it will do nothing as it's an empty function.
+#if (defined __APPLE__ || defined __linux__) && defined(KRR_PLATFORM_PC)
 #define HAS_BACKTRACE
 #include <execinfo.h>
 #endif
